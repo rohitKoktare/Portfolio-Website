569e422584bde74e995950eb42d8929edb54f5fc
rohitKoktare <rohitkoktarebugatti@gmai.com@example.com>
2025-01-03 11:04:02 +0530

    removed react tilt

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import React from 'react'
-import { Tilt } from 'react-tilt'
+import  Tilt  from "react-parallax-tilt"
 import { styles } from '../style'
 import { services } from '../constants'
 import {fadeIn, textVariant} from '../utils/motion'
