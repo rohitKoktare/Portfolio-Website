89816820e583cc976030ffcebdd82e58f335eb68
rohitKoktare <rohitkoktarebugatti@gmai.com@example.com>
2025-01-03 10:52:23 +0530

    first commitss

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
 // export default defineConfig({
 //   plugins: [react()],
 // })
 
-export default defineConfig({
-  plugins: [],
-  build: {
-    chunkSizeWarningLimit: 3000,  // Adjust the limit to your needs
-  }
-})
+
